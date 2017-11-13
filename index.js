6cef5b2f292b8587c7f28c31a7ab8b2b7ec9da5d
eunthebear <eun.david@gmail.com>
2017-11-13 01:49:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution in this file!
+var customerName;
+
+function upperCaseCustomerName() {
+  return customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  var bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(customer) {
+  bestCusomer = customer;
+}
+
